ae0ff6470bf15dd008f06899dfba756c6ade7fb5
lvsenlv <lvsen46000@163.com>
2016-12-26 11:29:20 +0800

    create project

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # vim
 some configuration about vim
+lvsenlv create this project by transplanting others 
+
+**********按键**********
+F3 NERDTree  目录树
+F9 Tlist     列出了当前文件中的所有宏, 全局变量, 函数名等
+F8 WMToggle  将NERDTree与Tlist合并显示
+    /*
+      若要修改显示的位置，修改winmanager.vim插件）
+      wincmd H   左侧
+      wincmd L   右侧
+    */
+F4           查找光标处，在指定的范围内的文件中查找
+:cw          quickfix窗口
+
+**********快捷键×××××××××
+ctrl 鼠标左键    查看函数、变量等等
+ctrl ]           查看函数、变量等等
+ctrl t           退回一步
+
+***技巧按键***按下ctrl \ 后，释放并按下对应字母，需要掌握按键时机，不可过快过慢
+ctrl \ g         查看光标处函数、变量等等
+ctrl \ c         查看光标处，调用该函数的文件
+
+
+**********安装***********
+
+1. 运行vim-master目录下的setup.sh
+2. 增添winmanager插件
+3. 增添minibufexplorer
+   minibufexplorer会与其他插件有点小冲突:
+       ctrl ] 然后 ctrl t 就会弹出多余方框、WMToggle显示方框异常等等；
+   解决方式: vim一个文件后，在Tlist中任意打开另一个文件即可
+4. 增添grep插件
+
+**********创建tags文件*************
+在工程文件最上层目录下，执行 ctags -R
+**********创建cscope文件***********
+在工程文件最上层目录下，执行 cscope -Rbq
+
+
+***********************************
+更多具体内容，参见
+http://blog.csdn.net/lhf_tiger/article/details/7216500
+
---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if which apt-get >/dev/null; then
+	sudo apt-get install -y git vim vim-gnome ctags cscope xclip astyle python-setuptools python-dev
+elif which yum >/dev/null; then
+	sudo yum install -y git vim gcc ctags cscope xclip astyle python-setuptools python-devel 	
+fi
+
+sudo easy_install -ZU autopep8 
+sudo ln -s /usr/bin/ctags /usr/local/bin/ctags
+
+if [ -d ~/vim ];then 
+    mv ~/vim ~/vim_old
+fi
+
+if [ -d ~/.vim ];then 
+    mv ~/.vim ~/.vim_old
+fi
+
+tar -xzvf ma6147_vundle_vim_git.tar.gz -C ~/ 
+mv -f ~/vim ~/.vim
+
+if [ -f ~/.vimrc ];then 
+    mv ~/.vimrc ~/.vimrc_old
+fi
+
+PATH=`pwd`
+
+cp -f $PATH/vimrc ~/.vimrc 
+cp -f $PATH/extra_plugins/*.vim ~/.vim/plugin/ 
+
+echo "
+
+
+Install bundle about vim .......... 
+It costs much time, please wait patiently
+
+
+The process bar is in left side of this window
+Symbol "+" : current bundle is being installed
+Symbol "!" : failed to install current bundle
+Rerun this script when the net is available
+" > vim_project
+
+vim vim_project -c "install_bundle" -c "q" -c "q"
+rm vim_project
+echo "
+**********************
+end of configuring vim
+**********************
+"
+
